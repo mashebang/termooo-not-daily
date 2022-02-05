cd833a507ccf7862622a9fdf0018d1da6db8e2ac
mashebang <mashebangisok@gmail.com>
2022-02-05 15:07:21 -0300

    get all words from random wiki and store it raw

--- scripts/getWords.js
+++ scripts/getWords.js
@@ -1,16 +1,50 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const path = require("puppeteer");
+const asyncFs = require("fs").promises;
 
+// get words and save
 (async () => {
+  const words = [];
+
+  try {
+    const addresses = Array(9)
+      .fill()
+      .map((e, k) =>
+        k == 0
+          ? "https://pt.wikwik.org/palavras5letras.htm"
+          : `https://pt.wikwik.org/palavras5letraspagina${k + 1}.htm`
+      );
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://pt.wikwik.org/palavras5letras.htm');
-    const wordsContainer = (await page.$$('.mm'))[0]
-    const wordsElements = Array.from(await wordsContainer.$$('a'))
-    const words = []
-    for (const word of wordsElements) {
-        const w = await (await word.getProperty('innerText')).jsonValue()
-        words.push(w)
+
+    for (const address of addresses) {
+      await page.goto(address);
+      const wordsContainer = (await page.$$(".mm"))[0];
+      const wordsElements = Array.from(await wordsContainer.$$("a"));
+      for (const word of wordsElements) {
+        const textElementHandle = await word.getProperty("innerText");
+        const currentWord = await textElementHandle.jsonValue();
+        words.push(currentWord);
+      }
     }
-    console.log(words)
+
     await browser.close();
-  })();
+  } catch (e) {
+    console.error("error on reading data from web: ", e);
+  }
+
+  try {
+    const wordsAsJson = JSON.stringify(words, null, "\t");
+    console.log(wordsAsJson);
+    await asyncFs.writeFile("data/words.json", wordsAsJson);
+  } catch (e) {
+    console.error("error on writing JSON to file: ", e);
+  }
+})();
+
+// data clean
+// (async () => {
+//     const words = JSON.parse((await asyncFs.readFile("../data/words.json")));
+
+//     console.log(words)
+// })()
